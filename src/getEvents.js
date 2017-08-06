84639c30b6df0543b3847475353ebf18fe558e25
Brett Wise <brettdwise@gmail.com>
2017-08-06 17:18:35 -0400

    Added logic to limit number events when there are too many to display on screen. Changed variable names for clarity

--- src/getEvents.js
+++ src/getEvents.js
@@ -17,7 +17,7 @@ export default class GetEvents extends React.Component {
           let AMPM, endHours, startHours, endAMPM, startAMPM;
           let startTime = new Date(event.start.dateTime);
           let endTime = new Date(event.end.dateTime);
-          let time = new Date();
+          let currentTime = new Date();
 
           function pad(n) {
             return (n < 10) ? ("0" + n) : n;
@@ -44,10 +44,12 @@ export default class GetEvents extends React.Component {
             event.end.time = `${endHours}:${pad(endTime.getMinutes())} ${endAMPM}`;
           }
 
-          if (endTime.getTime() < time.getTime()) {
+          if (currentTime.getTime() > startTime.getTime() && currentTime.getTime() < endTime.getTime()) {
+            event.class = "current";
+          } else if (endTime.getTime() < currentTime.getTime()) {
             event.class = "past";
           } else {
-            event.class = "current";
+            event.class = "future";
           }
           return event;
       }).filter( event => {
@@ -73,6 +75,21 @@ export default class GetEvents extends React.Component {
       return (
         <div className="past">
           <p className="event-title">No Scheduled Events Today</p>
+        </div>
+      )
+    } else if (this.state.events.length > 4){
+      return (
+        <div>
+          {this.state.events
+            .filter(event => event.class === "current" || event.class === "future")
+            .slice(0, 4)
+            .map(event =>
+            <div key={event.id} className={event.class}>
+              <p className="event-title">{event.summary}</p>
+              <p className="event-time">{event.start.time} - {event.end.time}</p>
+            </div>
+            )
+          }
         </div>
       )
     } else {
